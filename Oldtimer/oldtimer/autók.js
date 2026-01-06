e0372e6a790c1c6c8782433a73cb6262d1367a02
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-01-06 15:43:57 +0100

    Add files via upload

---
+++ Oldtimer/oldtimer/autók.js
@@ -0,0 +1,32 @@
+let autok=[
+    {
+        img: "./pics/trabant_700_400.jpg",
+        nev: "Trabant 601",
+        ar: "Ár: 2.500 Ft. / óra",
+        leiras: "A Trabant keletnémet gépkocsitípus, melyet a német VEB Sachsenring gyártott. a legelterjedtebb autó volt Kelet-Németországban.",
+    },
+    {
+        img: "./pics/moszkvics_700_400.jpg",
+        nev: "Moszkvics 408",
+        ar: "Ár: $24.99 / óra",
+        leiras: "A Moszkvics egy Moszkvában 1947 és 1968 között gyártott szovjet autómárka. A Moszkovszkij Zavod Maliolitrazsnih Avtomobilej (MZMA) készítette, akár a 130 km/h-s sebességre is képes.",
+    },
+    {
+        img: "./pics/delorean_700_400.jpg",
+        nev: "DeLorean DMC-12",
+        ar: "Ár: 12.000 huf / óra",
+        leiras: "Ezt a világhírű sportautót a John DeLorean által alapított DeLorean Motor Company gyártotta 1981-ben. A DeLorean egy módosított változata a Vissza a jövőbe filmtrilógiában szerepelt mint időgép.",
+    },
+    {
+        img: "./pics/cadillac_700_400.jpg",
+        nev: "Cadillac Fleetwood Brougham",
+        ar: "Ár: 15.000 Ft. / óra",
+        leiras: "Ezt a luxusautó az amerikai Cadillac gyár kapuján gurult ki 1977 és 1986 között. A Fleetwood jelzővel az összes lehetséges extrával felszerelt modellt illették.",
+    },
+    {
+        img: "./pics/ferrari_700_400.jpg",
+        nev: "Ferrari 348",
+        ar: "Ár: 18.000 huf / óra",
+        leiras: "A Ferrari 348-as 1989 és 1995 között készült, egyedi V8-as motorral ellátott sportautó. Ez volt a legutolsó modell, amit még maga Enzo Ferrari tervezett.",
+    }
+]
---
+++ Oldtimer/oldtimer/fejlec.js
@@ -0,0 +1,17 @@
+let fejlec=[
+    {
+        img: "./pics/moszkvics_fejlec_900_350.jpg",
+        cim: "Béreljen Klasszikust!",
+        leiras: "Nálunk minden régi óriást megtalál.",
+    },
+    {
+        img: "./pics/lada_fejlec_900_350.jpg",
+        cim: "A Keleti blokktól Hollywoodig",
+        leiras: "1960 és 1990 közötti autógyártás remekművei közül válogathat",
+    },
+    {
+        img: "./pics/polszki_fejlec_900_350.jpg",
+        cim: "Élje át gyerekkorát!",
+        leiras: "Ezek a matuzsálemek mindenki szívében egy parányi nosztalgiát hordoznak",
+    }
+]
