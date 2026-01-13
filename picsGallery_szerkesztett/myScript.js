e3d2aa72132ef3fd7872c3910eb321361323c793
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-01-13 16:24:56 +0100

    Add files via upload

--- picsGallery_szerkesztett/myScript.js
+++ picsGallery_szerkesztett/myScript.js
@@ -1,5 +1,5 @@
 const container=document.getElementById("main")
-
+let count=0;
 for (const element of figures) {
     if (element.cover) {
         container.innerHTML+=`
@@ -9,6 +9,7 @@ for (const element of figures) {
                 <p>${element.text?element.text:""}</p>
             </figure>
         `
+        count+=element.days?element.days:0
     }
     else{
         container.innerHTML+=`
@@ -18,5 +19,8 @@ for (const element of figures) {
                 <p>${element.text?element.text:""}</p>
             </figure>
         `
+        count+=element.days?element.days:0
     }
 }
+
+document.getElementById("footer").innerHTML+=`<p>Ezek a képek összesen ${count} nap, vagyis ${Math.round(count/365, 2)} év alatt készültek</p>`
